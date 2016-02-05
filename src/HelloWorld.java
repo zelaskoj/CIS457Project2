db23a60d3b661dcfceaa950fec671081a1e7c6fb
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-02-05 09:47:35 -0500

    we did it reddit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("I'm a winner");
 		System.out.println("Luigi sucks");
+		System.out.println("Marth is best character");
 	}
 }
