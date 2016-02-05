46e61bd56f4fd899b751ebb5c3e172c8699aefb9
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-02-05 13:36:17 -0500

    bye bye

--- src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-	public static void main(String args[]){
-		System.out.println("I'm a winner");
-		System.out.println("Luigi sucks");
-		System.out.println("Marth is best character");
-	}
-}
